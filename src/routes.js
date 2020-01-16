50b97907c1739a2ba56606cf361fe9444f07676c
Gustavo Angelo <gustavolz.angelo@gmail.com>
2020-01-15 23:00:05 -0300

    Model user created

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,16 @@
 import Router from 'express';
+import User from './app/models/Users';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello World' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Gustavo Angelo',
+    email: 'gustavo.angelo@elogroup.com.br',
+    password_hash: '123456789',
+  });
+
+  return res.json(user);
+});
 
 export default routes;
