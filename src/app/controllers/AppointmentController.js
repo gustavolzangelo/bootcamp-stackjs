7772159de7962e8b6f2d058a8b8932b03355d593
Gustavo Angelo <gustavolz.angelo@gmail.com>
2020-01-24 15:05:02 -0300

    handler added

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -88,6 +88,11 @@ class AppointmentController {
           as: 'provider',
           attributes: ['name', 'email'],
         },
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name'],
+        },
       ],
     });
 
@@ -110,7 +115,14 @@ class AppointmentController {
     await Mail.sendMail({
       to: `${appointment.provider.name} <${appointment.provider.email}>`,
       subject: 'Agendamento cancelado',
-      text: 'Voce teve um novo cancelamento.',
+      template: 'cancellation',
+      context: {
+        provider: appointment.provider.name,
+        user: appointment.user.name,
+        date: format(appointment.date, "'dia' dd 'de' MMMM', as' H:mm'h'", {
+          locale: pt,
+        }),
+      },
     });
 
     return res.json(appointment);
